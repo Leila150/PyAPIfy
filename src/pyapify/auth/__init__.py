948809635007ea5fc84042399a10197fe79e9e57
Leila150 <esguerradominguita@gmail.com>
2026-09-04 07:59:17 +0800

    Export JWT authentication provider

--- src/pyapify/auth/__init__.py
+++ src/pyapify/auth/__init__.py
@@ -1,1 +1,5 @@
 """Authentication system."""
+from .base import AuthProvider, APIKeyAuth, BearerAuth, BasicAuth
+from .jwt import JWTAuth
+
+__all__=['AuthProvider','APIKeyAuth','BearerAuth','BasicAuth','JWTAuth']
