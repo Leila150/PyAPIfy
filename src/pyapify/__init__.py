00c380452c5dc94ac9be9e6828493f6de7129fd1
Leila150 <esguerradominguita@gmail.com>
2026-09-04 07:59:22 +0800

    Expose JWT auth in public API

--- src/pyapify/__init__.py
+++ src/pyapify/__init__.py
@@ -4,7 +4,7 @@ from .http.response import HTTP, HTTPResponse
 from .http.request import Request, Headers, UploadFile
 from .http.status import Status
 from .validation.models import Model, ValidationError, Field
-from .auth.base import AuthProvider, APIKeyAuth, BearerAuth, BasicAuth
+from .auth import AuthProvider, APIKeyAuth, BearerAuth, BasicAuth, JWTAuth
 from .middleware import CORSMiddleware, SecurityHeadersMiddleware, RequestIDMiddleware, TimingMiddleware, RateLimitMiddleware
-__version__='0.3.0'
-__all__=['PyAPIfy','HTTP','HTTPResponse','Request','Headers','UploadFile','Status','Model','ValidationError','Field','Depends','depends','BackgroundTasks','AuthProvider','APIKeyAuth','BearerAuth','BasicAuth','CORSMiddleware','SecurityHeadersMiddleware','RequestIDMiddleware','TimingMiddleware','RateLimitMiddleware']
+__version__='0.3.1'
+__all__=['PyAPIfy','HTTP','HTTPResponse','Request','Headers','UploadFile','Status','Model','ValidationError','Field','Depends','depends','BackgroundTasks','AuthProvider','APIKeyAuth','BearerAuth','BasicAuth','JWTAuth','CORSMiddleware','SecurityHeadersMiddleware','RequestIDMiddleware','TimingMiddleware','RateLimitMiddleware']
