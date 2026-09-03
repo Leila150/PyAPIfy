79e1e0de01ff22693a2ec27e3d0038a8cb31c86d
Leila150 <esguerradominguita@gmail.com>
2026-09-04 07:42:45 +0800

    Export built-in middleware

--- src/pyapify/middleware/__init__.py
+++ src/pyapify/middleware/__init__.py
@@ -1,1 +1,3 @@
-"""Middleware pipeline."""
+"""Built-in middleware."""
+from .builtins import CORSMiddleware, SecurityHeadersMiddleware, RequestIDMiddleware, TimingMiddleware, RateLimitMiddleware
+__all__=['CORSMiddleware','SecurityHeadersMiddleware','RequestIDMiddleware','TimingMiddleware','RateLimitMiddleware']
