0d49a24e9a6355e00038ed02faf26bd0abe4604f
Leila150 <esguerradominguita@gmail.com>
2026-09-06 12:43:32 +0800

    Expose PyAPIfy logging helpers

--- src/pyapify/__init__.py
+++ src/pyapify/__init__.py
@@ -8,7 +8,8 @@ from .validation.models import Model, ValidationError, Field
 from .auth import AuthProvider, APIKeyAuth, BearerAuth, BasicAuth, JWTAuth
 from .middleware import CORSMiddleware, SecurityHeadersMiddleware, RequestIDMiddleware, TimingMiddleware, RateLimitMiddleware
 from .websocket import WebSocket, WebSocketDisconnect
+from .logging import get_logger, log
 
 http = HTTP
 __version__ = '0.3.1'
-__all__ = ['PyAPIfy','Plugin','PluginManager','HTTP','http','HTTPResponse','Request','Headers','UploadFile','Status','Model','ValidationError','Field','Depends','depends','BackgroundTasks','AuthProvider','APIKeyAuth','BearerAuth','BasicAuth','JWTAuth','WebSocket','WebSocketDisconnect','CORSMiddleware','SecurityHeadersMiddleware','RequestIDMiddleware','TimingMiddleware','RateLimitMiddleware']
+__all__ = ['PyAPIfy','Plugin','PluginManager','HTTP','http','HTTPResponse','Request','Headers','UploadFile','Status','Model','ValidationError','Field','Depends','depends','BackgroundTasks','AuthProvider','APIKeyAuth','BearerAuth','BasicAuth','JWTAuth','WebSocket','WebSocketDisconnect','CORSMiddleware','SecurityHeadersMiddleware','RequestIDMiddleware','TimingMiddleware','RateLimitMiddleware','get_logger','log']
