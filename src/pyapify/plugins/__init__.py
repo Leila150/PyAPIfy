622d65a5debc5da814ce20cae87e7b9ed311f88b
Leila150 <esguerradominguita@gmail.com>
2026-09-06 12:05:55 +0800

    Export public Plugin API

--- src/pyapify/plugins/__init__.py
+++ src/pyapify/plugins/__init__.py
@@ -1,1 +1,6 @@
-"""First-class PyAPIfy plugin system."""
+"""First-class PyAPIfy extension system."""
+
+from .api import Plugin
+from .manager import PluginManager
+
+__all__ = ["Plugin", "PluginManager"]
